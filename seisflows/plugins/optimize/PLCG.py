8a0d9c6d64efda90f2f7e84d54991bfe1f049492
gianmatharu <gian@ualberta.ca>
2017-11-02 17:15:24 -0600

    updated CG class

--- seisflows/plugins/optimize/PLCG.py
+++ seisflows/plugins/optimize/PLCG.py
@@ -81,8 +81,7 @@ class PLCG(LCG):
         """ Checks Eisenstat-Walker termination status
         """
         g0 = self.load('g_new')
-        #g1 = self.load('LCG/r')
-        g1 = g0 + ap
+        g1 = self.load('LCG/r')
 
         LHS = _norm(g1)
         RHS = _norm(g0)
@@ -91,14 +90,6 @@ class PLCG(LCG):
             print ' ETA:', self.eta
             print ' RATIO:', LHS/RHS
             print ''
-
-        #self.writer(
-        #    self.iter,
-        #    self.ilcg,
-        #    LHS,
-        #    RHS,
-        #    LHS/RHS,
-        #    eta1996)
 
         # check termination condition
         if LHS < self.eta * RHS:
@@ -116,32 +107,53 @@ class PLCG(LCG):
         # for comparison, calculates forcing term proposed by
         # Eisenstat & Walker 1996
         try:
-            a1 = 1
-            a2 = 0.5*(1 + np.sqrt(5))
-
-            g_new = _norm(self.load('g_new'))
-            g_old = _norm(self.load('g_old'))
-
-            # eta update
-            eta_k = a1*(g_new/g_old)**a2
-
-            # eta safeguard
-            eta_s = a1*self.eta**a2
-
-            # safeguard
-            if eta_s > 0.1:
-                if verbose:
-                    print ' ETA trial:', eta_k
-                    print ' ETA safeguard:', eta_s
-                    print ''
-                eta_k = max(eta_s, eta_k)
-
-            if eta_k < 1:
-                self.eta = eta_k
-
+            self.forcing_term_3(verbose=verbose)
         except IOError:
-            pass
+            print('Eta not updated')
 
+    def forcing_term_1(self, verbose=True):
+        """ Implements forcing term 1 in Eisenstat & Walker 1996
+        """
+        g_new = _norm(self.load('g_new'))
+        g_old = _norm(self.load('g_old'))
+
+        alpha = self.load('alpha')
+        Hdm_old = self.load('LCG/r_old') - g_old
+
+        if verbose:
+            print alpha
+
+        # eta_update and safeguard
+        eta_k = _norm(g_new - g_old - alpha*Hdm_old)/_norm(g_old)
+        eta_s = self.eta**(0.5*(1 + np.sqrt(5)))
+
+        eta_k = _safeguard(eta_k, eta_s, verbose=verbose)
+
+        if eta_k < 1:
+            self.eta = eta_k
+
+    def forcing_term_2(self, verbose=True):
+        """ Implements forcing term 2 in Eisenstat & Walker 1996
+        """
+        raise NotImplementedError
+
+    def forcing_term_3(self, verbose=True):
+        """ Implements forcing term 3 in Eisenstat & Walker 1996
+        """
+        a1 = 1
+        a2 = 0.5*(1 + np.sqrt(5))
+
+        g_new = _norm(self.load('g_new'))
+        g_old = _norm(self.load('g_old'))
+
+        # eta update and safeguard
+        eta_k = a1*(g_new/g_old)**a2
+        eta_s = a1*self.eta**a2
+
+        eta_k = _safeguard(eta_k, eta_s, verbose=verbose)
+
+        if eta_k < 1:
+            self.eta = eta_k
 
 ### utility functions
 
@@ -149,3 +161,14 @@ _done = 0
 
 def _norm(v):
     return float(np.linalg.norm(v))
+
+def _safeguard(eta_k, eta_s, verbose=True):
+    if eta_s > 0.1:
+        if verbose:
+            print ' ETA trial:', eta_k
+            print ' ETA safeguard:', eta_s
+            print ''
+            eta_k = max(eta_s, eta_k)
+        else:
+            pass
+    return eta_k
